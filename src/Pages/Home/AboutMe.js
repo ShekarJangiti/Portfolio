748ed7b12292bb3dd705fe21632db40ac8446293
Shekar J <shekarjangiti0310@gmail.com>
2025-03-03 13:11:01 -0600

    Update AboutMe.js

--- src/Pages/Home/AboutMe.js
+++ src/Pages/Home/AboutMe.js
@@ -1,8 +1,8 @@
-export default function AboutMe() {
+oexport default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/about-me.png" alt="About Me" />
+        <img src="./img/Hero.jpeg" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
